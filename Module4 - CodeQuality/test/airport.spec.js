7c0ee3a30b292b0fe2711ea7a37430788180f92e
Natallia Ratsinskaya <natallia_ratsinskaya@epam.com>
2020-04-08 17:31:57 +0300

    Module4 - small fixes

--- Module4 - CodeQuality/test/airport.spec.js
+++ Module4 - CodeQuality/test/airport.spec.js
@@ -9,7 +9,7 @@ const ExperimentalPlane = require('../Planes/ExperimentalPlane');
 const ExperimentalTypes = require('../models/ExperimentalTypes');
 const ClassificationLevel = require('../models/ClassificationLevel');
 
-describe('HereShouldBeGoodTestName', () => {
+describe('PlanesWhichAreStoredInTheAirport', () => {
 
     let planes = [
         new PassengerPlane('Boeing-737', 900, 12000, 60500, 164),
@@ -25,7 +25,7 @@ describe('HereShouldBeGoodTestName', () => {
         new MilitaryPlane('B-52 Stratofortress', 1000, 20000, 80000, MilitaryType.BOMBER),
         new MilitaryPlane('F-15', 1500, 12000, 10000, MilitaryType.FIGHTER),
         new MilitaryPlane('F-22', 1550, 13000, 11000, MilitaryType.FIGHTER),
-        new MilitaryPlane('C-130 Hercules', 650, 5000, 110000, MilitaryType.TYPE_TRANSPORT),
+        new MilitaryPlane('C-130 Hercules', 650, 5000, 110000, MilitaryType.TRANSPORT),
         new ExperimentalPlane("Bell X-14", 277, 482, 500, ExperimentalTypes.HIGH_ALTITUDE, ClassificationLevel.SECRET),
         new ExperimentalPlane("Ryan X-13 Vertijet", 560, 307, 500, ExperimentalTypes.VTOL, ClassificationLevel.TOP_SECRET)
     ];
@@ -33,10 +33,10 @@ describe('HereShouldBeGoodTestName', () => {
 
     it('MilitaryPlanesWithTransportType', () => {
         let airport = new Airport(planes);
-        let transportMilitaryPlanes = airport.getTransportMilitaryPlanes();
+        let transportMilitaryPlanes = airport.getMilitaryPlanes();
         let flag = false;
         for (let militaryPlane of transportMilitaryPlanes) {
-            if (militaryPlane.getMilitaryType() === "Transport") {
+            if (militaryPlane.getMilitaryType() === MilitaryType.TRANSPORT) {
                 flag = true;
                 break;
             }
@@ -44,13 +44,13 @@ describe('HereShouldBeGoodTestName', () => {
         assert.equal(flag, true);
     });
 
-    it('Check passenger plane with max capacity', () => {
+    it('PassengerPlaneWithMaxPassengersCapacity', () => {
         let airport = new Airport(planes);
         let expectedPlaneWithMaxPassengersCapacity = airport.getPassengerPlaneWithMaxPassengersCapacity();
         assert.isFalse(expectedPlaneWithMaxPassengersCapacity == planeWithMaxPassengerCapacity);
     });
 
-    it('GetPassengerPlaneWithMaxCapacity', () => {
+    it('PassengerPlaneWithMaxLoadCapacity', () => {
         let airport = new Airport(planes);
         airport.sortByMaxLoadCapacity();
         let planesSortedByMaxLoadCapacity = airport.getPlanes();
