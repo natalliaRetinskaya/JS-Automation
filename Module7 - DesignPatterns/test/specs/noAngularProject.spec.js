efb9c5af02036065e33606072c9684796b4e0dbb
Natallia Ratsinskaya <natallia_ratsinskaya@epam.com>
2020-05-08 18:12:40 +0300

    small fixes

--- Module7 - DesignPatterns/test/specs/noAngularProject.spec.js
+++ Module7 - DesignPatterns/test/specs/noAngularProject.spec.js
@@ -1,18 +1,19 @@
-browser.ignoreSynchrnization = true;
+  
 const expect = require("chai").expect;
-const until = require('selenium-webdriver');
-const BasePage = require("../utils/page_objects/base_page/base_page")
+const PageFactory = require("../utils/page_objects/pageFactory");
+const EC = protractor.ExpectedConditions;
+describe("Home page header", function () {
 
-describe("Count header links", function() {
-    beforeEach(function(){
-        let width = 1980;
-        let height = 600;
-        return browser.driver.manage().window().setSize(width, height);
+    beforeEach(function() {
+            browser.ignoreSynchronization = true;
+            //return browser.driver.manage().window().setSize(width, height);
     });
 
-    it("should have 6 menu items", async function() {
-        browser.waitForAngularEnabled(false);
-        const countOfNavigationButtons = await BasePage.open("https://www.epam.com").Header.navigationButtons.getCount();
+    it("should have 6 menu items", async function () {
+        await PageFactory.getPage("Home").open();
+        const firstNavigationButton = PageFactory.getPage("Home").Header.navigationButtons.collection.get(0);
+        await browser.wait(EC.elementToBeClickable(firstNavigationButton), 10000);
+        const countOfNavigationButtons = await PageFactory.getPage("Home").Header.navigationButtons.getCount();
         expect(countOfNavigationButtons).to.be.equal(6);
-    })
+    });
 });
--- Module7 - DesignPatterns/test/utils/page_objects/base_page/header.js
+++ Module7 - DesignPatterns/test/utils/page_objects/base_page/header.js
@@ -1,4 +1,4 @@
-  
+
 const Element = require("../base_elements/base_element");
 const Collection = require("../base_elements/base_collection");
 
@@ -6,6 +6,9 @@ class Header {
     constructor() {
         this.navigationButtons = new Collection("Navigation Buttons", "div.header .top-navigation__item");
         this.contactUsButton = new Element("Contact Us Button", ".cta-button-ui");
+    };
+    clickContacktUs(){
+        return this.contactUsButton.click();
     };
 };
 
