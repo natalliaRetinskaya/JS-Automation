dda8e9532f8dce067ca79f23aa86d546a63d5ecb
Natallia Ratsinskaya <natallia_ratsinskaya@epam.com>
2020-05-12 20:20:41 +0300

    additions

--- Module8 - SeleniumAdvanced/test/specs/AngularProject.spec.js
+++ Module8 - SeleniumAdvanced/test/specs/AngularProject.spec.js
@@ -4,7 +4,7 @@ const PageFactory = require("../utils/page_objects_forbes/pageFactory");
 const EC = protractor.ExpectedConditions;
 const {Builder, By, Key, until, driver} = require('selenium-webdriver');
 
-describe("Header controls are clickable", function() {
+describe("Header check", function() {
     // afterEach(function(){
     //     browser.close(); 
     // });
@@ -17,30 +17,23 @@ describe("Header controls are clickable", function() {
         await browser.actions().click(firstNavigationButton).perform();
         const currentUrl = await browser.getCurrentUrl();
         expect(currentUrl).contains("billionaires")
-        //await browser.wait(EC.urlContains('billionaires'), 5000);
 
-        //await browser.findElement(By.css('[aria-label=\"Billionaires\"]')).click();
-        // await browser.findElement(By.css('[aria-label=\"Innovation\"]')).click();
-        // await browser.findElement(By.css('[aria-label=\"Leadership\"]')).click();
-        // await browser.findElement(By.css('[aria-label=\"Money\"]')).click();
-        // await browser.findElement(By.css('[aria-label=\"Business\"]')).click();
-        // await browser.findElement(By.css('[data-title=\"Small Business\"]')).click();
-        // await browser.findElement(By.css('[aria-label=\"Lifestyle\"]')).click();
-        // await browser.findElement(By.css('[aria-label=\"Lists\"]')).click();
-        // await browser.findElement(By.css('[aria-label=\"Advisor\"]')).click();
-        // await browser.findElement(By.css('[data-title=\"Featured\"]')).click();
-        // await browser.findElement(By.css('[aria-label=\"Breaking\"]')).click();
-        // await browser.findElement(By.css('[data-title=\"More\"]')).click();
     });
 
-    // it("Search", async function() {
-    //     browser.waitForAngularEnabled(false);
-    //     await browser.get('https://www.forbes.com/');
-    //     await browser.findElement(By.css('[data-ga-track=\"U18 - Search Icon\"]')).click();
-    //     await browser.findElement(By.input('Search')).sendKeys('career', Key.RETURN);
+    it("Search", async function() {
+        browser.waitForAngularEnabled(false);
+        await PageFactory.getPage("Home").open();
+        await browser.findElement(By.css('[data-ga-track=\"U18 - Search Icon\"]')).click();
+        browser.actions().sendKeys('career', protractor.Key.ENTER).perform()
+        const currentUrl = await browser.getCurrentUrl();
+        expect(currentUrl).contains("career")
+        //await browser.findElement(By.input('Search')).sendKeys('career', Key.RETURN);
+        //await browser.findElement(By.input('Search')).sendKeys('career');
+        //browser.actions().sendKeys(protractor.Key.ENTER).perform()
 
-    // });
+    });
 
 });
+
 
 
