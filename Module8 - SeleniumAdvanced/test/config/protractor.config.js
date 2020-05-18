dc5f2ef25a3e29310463f7e007fdf76fc843e463
Natallia Ratsinskaya <natallia_ratsinskaya@epam.com>
2020-05-18 18:48:49 +0300

    module8

--- Module8 - SeleniumAdvanced/test/config/protractor.config.js
+++ Module8 - SeleniumAdvanced/test/config/protractor.config.js
@@ -1,4 +1,4 @@
-const yargs = require('yargs').argv;
+
 
 exports.config = {
     directConnect: true,
@@ -7,18 +7,16 @@ exports.config = {
         '../specs/AngularProject.spec.js',
         '../specs/noAngularProject.spec.js'
     ],
-    capabilities: {
-        'browserName': 'chrome',
-        chromeOptions: 
-        { 
-           args: ['--window-size=1800,900'] 
-        },
-        shardTestFiles: yargs.instances > 1,
-        maxInstances: yargs.instances || 3,
-    },
+    multiCapabilities:
+    [ 
+     {
+   'browserName': 'chrome',
+   'maxInstances': 5,
+     }],
     baseUrl: 'localhost',
     mochaOpts: {
         reporter: 'spec',
         timeout: 70000
-    }
+    },
+    SELENIUM_PROMISE_MANAGER: 0,
 };
--- Module8 - SeleniumAdvanced/test/specs/AngularProject.spec.js
+++ Module8 - SeleniumAdvanced/test/specs/AngularProject.spec.js
@@ -3,7 +3,6 @@ browser.ignoreSynchrnization = true;
 const expect = require("chai").expect;
 const PageFactory = require("../utils/page_objects_forbes/pageFactory");
 const EC = protractor.ExpectedConditions;
-const {Builder, By, Key, until, driver} = require('selenium-webdriver');
 
 
 describe("Header check", function () {
@@ -17,8 +16,9 @@ describe("Header check", function () {
         const firstNavigationButton = PageFactory.getPage("Home").Header.navigationButtons.collection.get(0);
         await browser.wait(EC.elementToBeClickable(firstNavigationButton), 10000);
         await browser.actions().click(firstNavigationButton).perform();
-        const currentUrl = await browser.getCurrentUrl();
-        expect(currentUrl).contains("billionaires")
+        browser.wait(EC.urlContains('billionaires'), 5000);
+        // const currentUrl = await browser.getCurrentUrl();
+        // expect(currentUrl).contains("billionaires")
 
     });
 
@@ -27,11 +27,23 @@ describe("Header check", function () {
         await PageFactory.getPage("Home").open();
         await browser.findElement(By.css('[data-ga-track=\"U18 - Search Icon\"]')).click();
         browser.actions().sendKeys('career', protractor.Key.ENTER).perform()
-        const currentUrl = await browser.getCurrentUrl();
-        expect(currentUrl).contains("career")
+        browser.wait(EC.urlContains('career'), 5000);
+        // const currentUrl = await browser.getCurrentUrl();
+        // expect(currentUrl).contains("career")
 
     });
 
+    it("Search for 'million' open appropriate page", async function () {
+        browser.waitForAngularEnabled(false);
+        await PageFactory.getPage("Home").open();
+        await browser.findElement(By.css('[data-ga-track=\"U18 - Search Icon\"]')).click();
+        browser.actions().sendKeys('million').perform()
+        browser.actions().mouseMove(element(by.css('[class=\"search-modal__submit\"]'))).mouseDown().mouseUp().perform()
+        browser.wait(EC.urlContains('million'), 5000);
+        // const currentUrl = await browser.getCurrentUrl();
+        // expect(currentUrl).contains("million")
+
+    });
 
     it("scroll to the footer", async function () {
         browser.waitForAngularEnabled(false);
@@ -41,13 +53,13 @@ describe("Header check", function () {
 
     });
 
-    it("zoom level to 80%", async function () {
-        browser.waitForAngularEnabled(false);
-        await PageFactory.getPage("Home").open();
-        browser.executeScript("document.body.style.zoom='80");
-        return browser.sleep(2000);
+    // it("zoom level to 80%", async function () {
+    //     browser.waitForAngularEnabled(false);
+    //     await PageFactory.getPage("Home").open();
+    //     browser.executeScript("document.body.style.zoom='80");
+    //     return browser.sleep(2000);
 
-    });
+    // });
 
     // it("highlight header element", async function() {
     //     browser.waitForAngularEnabled(false);
