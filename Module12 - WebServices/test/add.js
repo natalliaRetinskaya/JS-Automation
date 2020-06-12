d625ae2a865582d52fda366b17e63c7aeac8cdab
Natallia Ratsinskaya <natallia_ratsinskaya@epam.com>
2020-06-12 14:39:05 +0300

    small fixes

--- Module12 - WebServices/test/add.js
+++ Module12 - WebServices/test/add.js
@@ -8,6 +8,13 @@ describe('WebServices testing', () => {
 
     json.map(async (data) => {
 
+        it("Status message is OK", async function () {
+            let statusText
+            let responceJson = await axios.get('https://jsonplaceholder.typicode.com/users')
+            statusText= responceJson.statusText
+            expect(statusText).to.be.equal('OK')
+        })
+
         it("Status code is 200", async function () {
             let statusCode
             let responceJson = await axios.get('https://jsonplaceholder.typicode.com/users')
@@ -15,7 +22,14 @@ describe('WebServices testing', () => {
             expect(statusCode).to.be.equal(200)
         })
 
-        it("Header", async function () {
+        it("Header 'content-type' presents", async function () {
+            let header
+            let responceJson = await axios.get('https://jsonplaceholder.typicode.com/users')
+            header = responceJson.headers
+            expect(header).to.have.property('content-type');
+        })
+
+        it("Value of the 'content-type' Header", async function () {
             let header
             let responceJson = await axios.get('https://jsonplaceholder.typicode.com/users')
             header = responceJson.headers['content-type']
@@ -24,8 +38,6 @@ describe('WebServices testing', () => {
 
 
         it("Array of 10 Users", async function () {
-            
-           
             let responceJson = await axios.get('https://jsonplaceholder.typicode.com/users')
             let arrayOfUsers = responceJson.data
             expect(arrayOfUsers.length).to.be.equal(10)
